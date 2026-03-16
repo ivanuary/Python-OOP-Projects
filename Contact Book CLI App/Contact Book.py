de4bab8f93b59aa2fc139151197b3ff55e05bc38
ivanuary <248451981+ivanuary@users.noreply.github.com>
2026-03-16 21:41:29 +0800

    Did a basic menu program, no input validation yet for name, email and number

--- Contact Book CLI App/Contact Book.py
+++ Contact Book CLI App/Contact Book.py
@@ -12,12 +12,20 @@ class Contact():
         self.email = new_email
 
     def change_number(self, new_number):
-        self.number = self.number
+        self.number = new_number
 
 #Functions
 def view_contacts(arr:list):
     for i, item in enumerate(arr, start=0):
-        print(f"NAME: {arr[i].name} || EMAIL: {arr[i].email} || NUMBER: {arr[i].number}")
+        print(f"ENTRY #{i+1} || NAME: {arr[i].name} || EMAIL: {arr[i].email} || NUMBER: {arr[i].number}")
+
+def check_int():
+    while True:
+        try:
+            num_int = int(input("ENTER NUMBER: "))
+            return num_int
+        except ValueError:
+            print("Not a valid number! Please Try Again")
 
 #Start
 contacts = []
@@ -42,7 +50,21 @@ CONTACT BOOK
         view_contacts(contacts)
     elif menu_choice == '3':
         print("============")
+        view_contacts(contacts)
+        print("============")
+        print("""SELECT ENTRY TO EDIT...""")
+        while True:
+            entry_edit = check_int()
+            if entry_edit > len(contacts) or entry_edit < 1:
+                print("INVALID INPUT, TRY AGAIN\n")
+            else:
+                contacts[entry_edit-1].change_name(input("Enter New Name: "))
+                contacts[entry_edit-1].change_email(input("Enter New Email: "))
+                contacts[entry_edit-1].change_number(input("Enter New Number: "))
+                break
     elif menu_choice == '4':
         print("============")
+        quit()
     else:
         print("============")
+        print("INVALID INPUT, PLEASE TRY AGAIN")
